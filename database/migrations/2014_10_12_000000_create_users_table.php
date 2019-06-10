5b68cfdd92b7bb0726908d91272356f64740714a
Dsalazar93 <salazar.gonzalez.d@gmail.com>
2019-06-09 20:24:39 -0500

    Añadiendo valor por defecto a la columna de type en users para evitar confilctos con posgres de heroku

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('type', ['parent', 'child']);
+            $table->enum('type', ['parent', 'child'])->default('parent');
             $table->rememberToken();
             $table->timestamps();
         });
